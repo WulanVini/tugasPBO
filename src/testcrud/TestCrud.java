dfb59320124f3b7cbafbe98a46356fb16ecfb724
ASUS <ASUS@10.4.6.17>
2024-11-11 12:39:10 +0800

    Init project

---
+++ src/testcrud/TestCrud.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testcrud;
+
+/**
+ *
+ * @author ASUS
+ */
+public class TestCrud {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
